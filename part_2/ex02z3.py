58a630fe03def2da29558c3ee5c18a623d15a320
Ties <tg.klappe@gmail.com>
2021-12-21 21:06:18 +0100

    Finish Part a

--- part_2/ex02z3.py
+++ part_2/ex02z3.py
@@ -50,6 +50,29 @@ s.add(village_b_stock[0] == INITIAL_PACKAGES_IN_VILLAGES)
 s.add(village_c_stock[0] == INITIAL_PACKAGES_IN_VILLAGES)
 s.add(village_d_stock[0] == INITIAL_PACKAGES_IN_VILLAGES)
 
+def max_capacity(village):
+    if village == VILLAGE_A:
+        return MAX_VILLAGE_A_CAPACITY
+    elif village == VILLAGE_B:
+        return MAX_VILLAGE_B_CAPACITY
+    elif village == VILLAGE_C:
+        return MAX_VILLAGE_C_CAPACITY
+    elif village == VILLAGE_D:
+        return MAX_VILLAGE_D_CAPACITY
+
+    print(f'Unknown village {village}') 
+
+def current_stock(village, current_iteration):
+    if village == VILLAGE_A:
+        return village_a_stock[current_iteration]
+    elif village == VILLAGE_B:
+        return village_b_stock[current_iteration]
+    elif village == VILLAGE_C:
+        return village_c_stock[current_iteration]
+    elif village == VILLAGE_D:
+        return village_d_stock[current_iteration]
+
+    print(f'Unknown village {village}') 
 
 def previous_stock(village, current_iteration):
     if village == VILLAGE_A:
@@ -67,19 +90,22 @@ def previous_stock(village, current_iteration):
 i = 0
 
 while True:
-    i += 1
-
-    if i == 33:
+    if i >= 33 and not (sat == s.check()):
+        print(f'Found unsatisfiable assignment after {i-1} iterations')
         break
+
+    i += 1
 
     next_truck_position = Int(f'Tp{i}')
     next_truck_stock = Int(f'St{i}')
+    previous_truck_stock = truck_stock[i - 1]
 
     next_village_a_stock = Int(f'Sa{i}')
     next_village_b_stock = Int(f'Sb{i}')
     next_village_c_stock = Int(f'Sc{i}')
     next_village_d_stock = Int(f'Sd{i}')
 
+    # Never drop below 0, or some village dies
     s.add(And(
         next_truck_stock >= 0,
         next_village_a_stock >= 0,
@@ -95,9 +121,11 @@ while True:
     village_c_stock.append(next_village_c_stock)
     village_d_stock.append(next_village_d_stock)
 
+    # Determine our current location
     for location in LOCATIONS:
         next_location_choices = []
         
+        # Determine the locations to which we can move in the next step
         for next_location, cost in enumerate(TRAVEL_TIMES[location]):
             # No route
             if cost == -1:
@@ -106,47 +134,30 @@ while True:
             next_location_consequences = []
             next_location_consequences.append(next_truck_position == next_location)
 
-            if not next_location == VILLAGE_A:
-                next_location_consequences.append(next_village_a_stock == previous_stock(VILLAGE_A, i) - cost)
-            else:
-                next_location_consequences.append(previous_stock(VILLAGE_A, i) - cost >= 0)
-                next_location_consequences.append(next_village_a_stock <= MAX_VILLAGE_A_CAPACITY)
-                next_location_consequences.append(next_village_a_stock <= previous_stock(VILLAGE_A, i) - cost + truck_stock[i - 1])
-                next_location_consequences.append(next_truck_stock == truck_stock[i - 1] - (next_village_a_stock - previous_stock(VILLAGE_A, i) + cost))
+            # We are now in a village that we can provide some supplies
+            if next_location in VILLAGES:
+                next_location_consequences.append(previous_stock(next_location, i) - cost >= 0)
+                next_location_consequences.append(current_stock(next_location, i) <= max_capacity(next_location))
+                next_location_consequences.append(current_stock(next_location, i) <= previous_stock(next_location, i) - cost + previous_truck_stock)
+                next_location_consequences.append(next_truck_stock == previous_truck_stock - (current_stock(next_location, i) - previous_stock(next_location, i) + cost))
 
-            if not next_location == VILLAGE_B:
-                next_location_consequences.append(next_village_b_stock == previous_stock(VILLAGE_B, i) - cost)
-            else:
-                next_location_consequences.append(previous_stock(VILLAGE_B, i) - cost >= 0)
-                next_location_consequences.append(next_village_b_stock <= MAX_VILLAGE_B_CAPACITY)
-                next_location_consequences.append(next_village_b_stock <= previous_stock(VILLAGE_B, i) - cost + truck_stock[i - 1])
-                next_location_consequences.append(next_truck_stock == truck_stock[i - 1] - (next_village_b_stock - previous_stock(VILLAGE_B, i) + cost))
+            # The next location is S, so we can refill the truck to full capacity
+            elif next_location == LOCATION_S:
+                next_location_consequences.append(next_truck_stock == MAX_TRUCK_CAPACITY)
 
-            if not next_location == VILLAGE_C:
-                next_location_consequences.append(next_village_c_stock == previous_stock(VILLAGE_C, i) - cost)
-            else:
-                next_location_consequences.append(previous_stock(VILLAGE_C, i) - cost >= 0)
-                next_location_consequences.append(next_village_c_stock <= MAX_VILLAGE_C_CAPACITY)
-                next_location_consequences.append(next_village_c_stock <= previous_stock(VILLAGE_C, i) - cost + truck_stock[i - 1])
-                next_location_consequences.append(next_truck_stock == truck_stock[i - 1] - (next_village_c_stock - previous_stock(VILLAGE_C, i) + cost))
+            # Every other village cusomes the cost
+            for village in VILLAGES:
+                if village == next_location:
+                    continue
 
-            if not next_location == VILLAGE_D:
-                next_location_consequences.append(next_village_d_stock == previous_stock(VILLAGE_D, i) - cost)
-            else:
-                next_location_consequences.append(previous_stock(VILLAGE_D, i) - cost >= 0)
-                next_location_consequences.append(next_village_d_stock <= MAX_VILLAGE_D_CAPACITY)
-                next_location_consequences.append(next_village_d_stock <= previous_stock(VILLAGE_D, i) - cost + truck_stock[i - 1])
-                next_location_consequences.append(next_truck_stock == truck_stock[i - 1] - (next_village_d_stock - previous_stock(VILLAGE_D, i) + cost))
+                next_location_consequences.append(current_stock(village, i) == previous_stock(village, i) - cost)
 
-            if next_location == LOCATION_S:
-                next_location_consequences.append(next_truck_stock == 250)
-
+            # Add all consequences of moving to this location
             next_location_choices.append(And(*next_location_consequences))
 
+        # If we are in this location, there are a some choices to be made.
         s.add(Implies(truck_position[i - 1] == location, Or(*next_location_choices)))
 
     truck_position.append(next_truck_position)
 
-print(f'{s.check()}')
-# print(f'{s.model()}')
-# print(f'{s}')
+print('Terminated')
